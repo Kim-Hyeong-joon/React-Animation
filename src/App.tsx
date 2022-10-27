b6ea5f11c607b51b2d55f59a58736bdb193d8e93
Hyeong-Joon <gudwns1020@naver.com>
2022-10-28 01:35:25 +0900

    MotionValues part Three

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import styled from "styled-components";
-import { delay, motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  delay,
+  motion,
+  useMotionValue,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 import { useEffect, useRef } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -27,20 +22,27 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  position: fixed;
+  top: 0;
 `;
-
-const boxVariants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: "100px" },
-};
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-300, 0, 300], [2, 1, 0.1]);
-
+  const rotateZ = useTransform(x, [-300, 300], [-180, 180]);
+  const gradient = useTransform(
+    x,
+    [-300, 0, 300],
+    [
+      "linear-gradient(135deg, rgb(36, 0, 238), rgb(0, 107, 238))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 159), rgb(24, 238, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1.5]);
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
